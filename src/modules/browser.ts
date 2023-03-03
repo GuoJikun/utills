953450cdb49b074075a0a4b7d62896fdc8a3ca9c
GuoJikun <guojikun486546@163.com>
2023-03-03 17:10:31 +0800

    ✨ feat: 重构代码

--- src/modules/browser.ts
+++ src/modules/browser.ts
@@ -1,47 +1,54 @@
-export const currentHref = () => window.location.href;
+export class Browser {
+  public static getStyle(el: HTMLElement, ruleName: string) {
+    return getComputedStyle(el).getPropertyValue(ruleName);
+  }
 
-/**添加class 名称 */
-export const addClass = (el: string | HTMLElement, className = "") => {
-  if (el instanceof HTMLElement) {
-    const oldClass = el.getAttribute("class") || "";
-    el.setAttribute(
-      "class",
-      oldClass ? [oldClass, className].join(" ") : className
-    );
-  } else {
-    const target = document.querySelector(el as any);
-    if (target) {
-      const oldClass = target.getAttribute("class");
-      target.setAttribute(
+  public static addClass(el: string | HTMLElement, className = "") {
+    if (el instanceof HTMLElement) {
+      const oldClass = el.getAttribute("class") || "";
+      el.setAttribute(
         "class",
         oldClass ? [oldClass, className].join(" ") : className
       );
+    } else {
+      const target = document.querySelector(el as any);
+      if (target) {
+        const oldClass = target.getAttribute("class");
+        target.setAttribute(
+          "class",
+          oldClass ? [oldClass, className].join(" ") : className
+        );
+      }
     }
   }
-};
-/**删除元素上的class 名称 */
-export const removeClass = (el: string | HTMLElement, className: string) => {
-  const removeClassList = className.split(" ");
-  if (el instanceof HTMLElement) {
-    const oldClass = el.getAttribute("class")?.split(" ") || [];
-    el.setAttribute(
-      "class",
-      oldClass.filter((c: string) => !removeClassList.includes(c)).join(" ")
-    );
-  } else {
-    const target = document.querySelector(el as any);
-    if (target) {
-      const oldClass = target.getAttribute("class")?.split(" ") || [];
-      target.setAttribute(
+
+  public static removeClass(el: string | HTMLElement, className: string) {
+    const removeClassList = className.split(" ");
+    if (el instanceof HTMLElement) {
+      const oldClass = el.getAttribute("class")?.split(" ") || [];
+      el.setAttribute(
         "class",
         oldClass.filter((c: string) => !removeClassList.includes(c)).join(" ")
       );
+    } else {
+      const target = document.querySelector(el as any);
+      if (target) {
+        const oldClass = target.getAttribute("class")?.split(" ") || [];
+        target.setAttribute(
+          "class",
+          oldClass.filter((c: string) => !removeClassList.includes(c)).join(" ")
+        );
+      }
     }
   }
-};
 
-export default {
-  currentHref,
-  addClass,
-  removeClass,
-};
+  public static currentHref() {
+    return window.location.href;
+  }
+}
+
+export const currentHref = Browser.currentHref;
+export const addClass = Browser.addClass;
+export const removeClass = Browser.removeClass;
+
+export default Browser;
